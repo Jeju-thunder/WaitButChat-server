6c26811504d16603f55b2122a573d2872ec64888
Kukjun Lee <63409722+kukjun@users.noreply.github.com>
2025-04-13 02:02:02 +0900

    fix: migrate register_blacklist

---
+++ prisma/migrations/20250412165852_/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `period_days` on the `register_blacklist` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "register_blacklist" DROP COLUMN "period_days";
